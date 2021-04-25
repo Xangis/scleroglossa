cba428e5f900f2ea92817f6976e34493cf1634f5
Jason Champion <jchampion@zetacentauri.com>
2021-04-25 16:26:44 -0700

    Add menu bar, start building functions.

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -43,25 +43,37 @@
             this.btnBack = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
             this.btnForward = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.historyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bookmarksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.windowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // geckoWebBrowser1
             // 
             this.geckoWebBrowser1.ConsoleMessageEventReceivesConsoleLogCalls = true;
             this.geckoWebBrowser1.FrameEventsPropagateToMainWindow = false;
-            this.geckoWebBrowser1.Location = new System.Drawing.Point(3, 62);
-            this.geckoWebBrowser1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.geckoWebBrowser1.Location = new System.Drawing.Point(4, 107);
+            this.geckoWebBrowser1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.geckoWebBrowser1.Name = "geckoWebBrowser1";
-            this.geckoWebBrowser1.Size = new System.Drawing.Size(1000, 498);
+            this.geckoWebBrowser1.Size = new System.Drawing.Size(1333, 582);
             this.geckoWebBrowser1.TabIndex = 0;
             this.geckoWebBrowser1.UseHttpActivityObserver = false;
             // 
             // btnBookmark
             // 
             this.btnBookmark.Image = ((System.Drawing.Image)(resources.GetObject("btnBookmark.Image")));
-            this.btnBookmark.Location = new System.Drawing.Point(616, 12);
+            this.btnBookmark.Location = new System.Drawing.Point(821, 47);
+            this.btnBookmark.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnBookmark.Name = "btnBookmark";
-            this.btnBookmark.Size = new System.Drawing.Size(22, 22);
+            this.btnBookmark.Size = new System.Drawing.Size(29, 27);
             this.btnBookmark.TabIndex = 26;
             this.btnBookmark.UseVisualStyleBackColor = true;
             this.btnBookmark.Click += new System.EventHandler(this.BtnBookmark_Click);
@@ -70,18 +82,19 @@
             // 
             this.cbBookmarks.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbBookmarks.FormattingEnabled = true;
-            this.cbBookmarks.Location = new System.Drawing.Point(252, 12);
-            this.cbBookmarks.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbBookmarks.Location = new System.Drawing.Point(336, 47);
+            this.cbBookmarks.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbBookmarks.Name = "cbBookmarks";
-            this.cbBookmarks.Size = new System.Drawing.Size(360, 23);
+            this.cbBookmarks.Size = new System.Drawing.Size(479, 30);
             this.cbBookmarks.TabIndex = 25;
             // 
             // btnOpen
             // 
             this.btnOpen.Image = global::Scleroglossa.Resources.openfolder_orange;
-            this.btnOpen.Location = new System.Drawing.Point(132, 12);
+            this.btnOpen.Location = new System.Drawing.Point(176, 47);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnOpen.Name = "btnOpen";
-            this.btnOpen.Size = new System.Drawing.Size(22, 22);
+            this.btnOpen.Size = new System.Drawing.Size(29, 27);
             this.btnOpen.TabIndex = 21;
             this.btnOpen.UseVisualStyleBackColor = true;
             this.btnOpen.Click += new System.EventHandler(this.BtnOpen_Click);
@@ -89,9 +102,10 @@
             // btnHome
             // 
             this.btnHome.Image = global::Scleroglossa.Resources.home;
-            this.btnHome.Location = new System.Drawing.Point(106, 12);
+            this.btnHome.Location = new System.Drawing.Point(141, 47);
+            this.btnHome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnHome.Name = "btnHome";
-            this.btnHome.Size = new System.Drawing.Size(22, 22);
+            this.btnHome.Size = new System.Drawing.Size(29, 27);
             this.btnHome.TabIndex = 20;
             this.btnHome.UseVisualStyleBackColor = true;
             this.btnHome.Click += new System.EventHandler(this.BtnHome_Click);
@@ -99,9 +113,10 @@
             // btnAbout
             // 
             this.btnAbout.Image = global::Scleroglossa.Resources.question2;
-            this.btnAbout.Location = new System.Drawing.Point(224, 12);
+            this.btnAbout.Location = new System.Drawing.Point(299, 47);
+            this.btnAbout.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnAbout.Name = "btnAbout";
-            this.btnAbout.Size = new System.Drawing.Size(22, 22);
+            this.btnAbout.Size = new System.Drawing.Size(29, 27);
             this.btnAbout.TabIndex = 24;
             this.btnAbout.UseVisualStyleBackColor = true;
             this.btnAbout.Click += new System.EventHandler(this.BtnAbout_Click);
@@ -109,9 +124,10 @@
             // btnViewSource
             // 
             this.btnViewSource.Image = global::Scleroglossa.Resources.code;
-            this.btnViewSource.Location = new System.Drawing.Point(182, 12);
+            this.btnViewSource.Location = new System.Drawing.Point(243, 47);
+            this.btnViewSource.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnViewSource.Name = "btnViewSource";
-            this.btnViewSource.Size = new System.Drawing.Size(38, 22);
+            this.btnViewSource.Size = new System.Drawing.Size(51, 27);
             this.btnViewSource.TabIndex = 23;
             this.btnViewSource.UseVisualStyleBackColor = true;
             this.btnViewSource.Click += new System.EventHandler(this.BtnViewSource_Click);
@@ -119,9 +135,10 @@
             // btnPrint
             // 
             this.btnPrint.Image = global::Scleroglossa.Resources.icon_print;
-            this.btnPrint.Location = new System.Drawing.Point(156, 12);
+            this.btnPrint.Location = new System.Drawing.Point(208, 47);
+            this.btnPrint.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnPrint.Name = "btnPrint";
-            this.btnPrint.Size = new System.Drawing.Size(22, 22);
+            this.btnPrint.Size = new System.Drawing.Size(29, 27);
             this.btnPrint.TabIndex = 22;
             this.btnPrint.UseVisualStyleBackColor = true;
             this.btnPrint.Click += new System.EventHandler(this.BtnPrint_Click);
@@ -129,9 +146,10 @@
             // btnGo
             // 
             this.btnGo.Image = global::Scleroglossa.Resources.right;
-            this.btnGo.Location = new System.Drawing.Point(981, 37);
+            this.btnGo.Location = new System.Drawing.Point(1308, 78);
+            this.btnGo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnGo.Name = "btnGo";
-            this.btnGo.Size = new System.Drawing.Size(22, 22);
+            this.btnGo.Size = new System.Drawing.Size(29, 27);
             this.btnGo.TabIndex = 15;
             this.btnGo.UseVisualStyleBackColor = true;
             this.btnGo.Click += new System.EventHandler(this.BtnGo_Click);
@@ -139,27 +157,30 @@
             // btnRefresh
             // 
             this.btnRefresh.Image = global::Scleroglossa.Resources.refresh;
-            this.btnRefresh.Location = new System.Drawing.Point(80, 12);
+            this.btnRefresh.Location = new System.Drawing.Point(107, 47);
+            this.btnRefresh.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(22, 22);
+            this.btnRefresh.Size = new System.Drawing.Size(29, 27);
             this.btnRefresh.TabIndex = 19;
             this.btnRefresh.UseVisualStyleBackColor = true;
             this.btnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
             // 
             // txtUrlBar
             // 
-            this.txtUrlBar.Location = new System.Drawing.Point(3, 38);
+            this.txtUrlBar.Location = new System.Drawing.Point(4, 79);
+            this.txtUrlBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtUrlBar.Name = "txtUrlBar";
-            this.txtUrlBar.Size = new System.Drawing.Size(974, 20);
+            this.txtUrlBar.Size = new System.Drawing.Size(1297, 22);
             this.txtUrlBar.TabIndex = 14;
             this.txtUrlBar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtUrlBar_KeyPress);
             // 
             // btnBack
             // 
             this.btnBack.Image = global::Scleroglossa.Resources.left;
-            this.btnBack.Location = new System.Drawing.Point(3, 12);
+            this.btnBack.Location = new System.Drawing.Point(4, 47);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(22, 22);
+            this.btnBack.Size = new System.Drawing.Size(29, 27);
             this.btnBack.TabIndex = 16;
             this.btnBack.UseVisualStyleBackColor = true;
             this.btnBack.Click += new System.EventHandler(this.BtnBack_Click);
@@ -167,9 +188,10 @@
             // btnStop
             // 
             this.btnStop.Image = global::Scleroglossa.Resources.stop2;
-            this.btnStop.Location = new System.Drawing.Point(54, 12);
+            this.btnStop.Location = new System.Drawing.Point(72, 47);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(22, 22);
+            this.btnStop.Size = new System.Drawing.Size(29, 27);
             this.btnStop.TabIndex = 18;
             this.btnStop.UseVisualStyleBackColor = true;
             this.btnStop.Click += new System.EventHandler(this.BtnStop_Click);
@@ -177,18 +199,95 @@
             // btnForward
             // 
             this.btnForward.Image = global::Scleroglossa.Resources.right;
-            this.btnForward.Location = new System.Drawing.Point(28, 12);
+            this.btnForward.Location = new System.Drawing.Point(37, 47);
+            this.btnForward.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnForward.Name = "btnForward";
-            this.btnForward.Size = new System.Drawing.Size(22, 22);
+            this.btnForward.Size = new System.Drawing.Size(29, 27);
             this.btnForward.TabIndex = 17;
             this.btnForward.UseVisualStyleBackColor = true;
             this.btnForward.Click += new System.EventHandler(this.BtnForward_Click);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.historyToolStripMenuItem,
+            this.bookmarksToolStripMenuItem,
+            this.windowToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1344, 33);
+            this.menuStrip1.TabIndex = 27;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(58, 29);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(65, 29);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // historyToolStripMenuItem
+            // 
+            this.historyToolStripMenuItem.Name = "historyToolStripMenuItem";
+            this.historyToolStripMenuItem.Size = new System.Drawing.Size(85, 29);
+            this.historyToolStripMenuItem.Text = "History";
+            // 
+            // bookmarksToolStripMenuItem
+            // 
+            this.bookmarksToolStripMenuItem.Name = "bookmarksToolStripMenuItem";
+            this.bookmarksToolStripMenuItem.Size = new System.Drawing.Size(117, 29);
+            this.bookmarksToolStripMenuItem.Text = "Bookmarks";
+            // 
+            // windowToolStripMenuItem
+            // 
+            this.windowToolStripMenuItem.Name = "windowToolStripMenuItem";
+            this.windowToolStripMenuItem.Size = new System.Drawing.Size(94, 29);
+            this.windowToolStripMenuItem.Text = "Window";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(69, 29);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(65, 29);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.BtnAbout_Click);
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1008, 561);
+            this.ClientSize = new System.Drawing.Size(1344, 690);
             this.Controls.Add(this.btnBookmark);
             this.Controls.Add(this.cbBookmarks);
             this.Controls.Add(this.btnOpen);
@@ -203,10 +302,14 @@
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnForward);
             this.Controls.Add(this.geckoWebBrowser1);
+            this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MainForm";
             this.Text = "Scleroglossa Browser";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -228,6 +331,16 @@
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Button btnStop;
         private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem historyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bookmarksToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem windowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
     }
 }
 
